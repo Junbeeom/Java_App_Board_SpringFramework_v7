9ba6c15fc6d196419ab0e5cec89aee20ad5f4837
Junbeeom <wns_p@naver.com>
2022-08-11 17:24:00 +0900

    등록할때 PRG적용

--- src/main/java/com/example/SpringFramework/board/web/basic/BasicBoardController.java
+++ src/main/java/com/example/SpringFramework/board/web/basic/BasicBoardController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -59,19 +60,14 @@ public class BasicBoardController {
         return "basic/board";
     }
 
+    //등록 폼
     @PostMapping("/add")
-    public String addItemV2(@ModelAttribute("board") Board board, Model model) {
-        System.out.println(board.getName());
-        System.out.println(board.getContent());
-        System.out.println("********************************");
-        memoryBoardRepository2.create(board);
-        //model.addAttribute("board", board);//자동추가, 생략가능 " "이 " " 대체
-        System.out.println(board.getId());
-        System.out.println(board.getName());
-        System.out.println(board.getContent());
-        System.out.println("********************************");
+    public String add(@ModelAttribute Board board, RedirectAttributes redirectAttributes) {
+        Board createBoard = memoryBoardRepository2.create(board);
+        redirectAttributes.addAttribute("boardId", createBoard.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/boards/{boardId}";
 
-        return "basic/board";
     }
 
     //수정 폼 보기.
