da5cdb13936cab14e9e60149bc19e54ed3330870
Junbeeom <wns_p@naver.com>
2022-08-12 20:28:29 +0900

    session 기능 구현 및 test code 검증 완료

--- src/main/java/com/example/SpringFramework/board/web/HomeController.java
+++ src/main/java/com/example/SpringFramework/board/web/HomeController.java
@@ -1,13 +1,42 @@
 package com.example.SpringFramework.board.web;
 
+import com.example.SpringFramework.board.domain.member.Member;
+import com.example.SpringFramework.board.repository.member.MemoryMemberRepository;
+import com.example.SpringFramework.board.web.session.SessionManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemoryMemberRepository memoryMemberRepository;
+    private final SessionManager sessionManager;
+
+    //@GetMapping("/")
     public String home() {
         return "home";
     }
+
+    @GetMapping("/")
+    public String homeLogin(HttpServletRequest request, Model model) {
+
+        //세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
+
+        //로그인
+        if(member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+
 }
--- src/main/java/com/example/SpringFramework/board/web/login/LoginController.java
+++ src/main/java/com/example/SpringFramework/board/web/login/LoginController.java
@@ -2,6 +2,7 @@ package com.example.SpringFramework.board.web.login;
 
 import com.example.SpringFramework.board.domain.login.LoginService;
 import com.example.SpringFramework.board.domain.member.Member;
+import com.example.SpringFramework.board.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,12 +12,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
 
     private final LoginService loginService;
+    private final SessionManager sessionManager;
 
     //로그인 폼 보여주는 화면
     @GetMapping("/login")
@@ -26,7 +31,7 @@ public class LoginController {
 
     //실제 로그인 처리
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -39,7 +44,20 @@ public class LoginController {
         }
 
         //로그인 성공 처리 TODO
+
+        //세션 관리자를 통해 세션을 생성하고, 회원 데이터 보관
+        sessionManager.createSession(loginMember, response);
+
+        //쿠기에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+
+
         //로그인 되면 home 으로 보냄.
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        sessionManager.expire(request);
         return "redirect:/";
     }
 }
