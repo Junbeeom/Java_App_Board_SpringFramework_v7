64866f2f46a3535603295226aaaf0ec736573efa
Junbeeom <wns_p@naver.com>
2022-08-13 10:06:28 +0900

    미인증 사용자 check를 위한 interceptor logic 구현 완료

--- src/main/java/com/example/SpringFramework/board/WebConfig.java
+++ src/main/java/com/example/SpringFramework/board/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.SpringFramework.board;
 
 import com.example.SpringFramework.board.web.interceptor.LogInterceptor;
+import com.example.SpringFramework.board.web.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,5 +18,14 @@ public class WebConfig implements WebMvcConfigurer {
                 // /*여기 하위는 전부다
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/", "/members/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error"
+                );
     }
 }
