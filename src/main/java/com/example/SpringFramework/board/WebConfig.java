abe2d7e64765fedb1d2565c2e9423164df71b621
Junbeeom <wns_p@naver.com>
2022-08-13 09:53:07 +0900

    logInterceptor 구현 완료 login 인증 logic 구현 해야함

---
+++ src/main/java/com/example/SpringFramework/board/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.SpringFramework.board;
+
+import com.example.SpringFramework.board.web.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+
+                // /*여기 하위는 전부다
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
+}
